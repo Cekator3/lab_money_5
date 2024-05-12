c7397b322907ee561454ecb81770ab02ae570eca
cekator3 <hamsterdreams@inbox.ru>
2024-05-12 21:31:01 +0500

    ...

--- lib/database.dart
+++ lib/database.dart
@@ -1,40 +1,56 @@
 import 'dart:io';
-import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 
 // Subsystem for initializing and getting object for interaction with database
 class DB
 {
+  static const _DB_NAME = 'testDb.db';
+
+  static Future<String> _getDatabaseFilepath() async
+  {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    return path.join(documentsDirectory.path, _DB_NAME);
+  }
+
+  static Future<void> _onCreate(Database db, int version) async
+  {
+    return await db.execute('''
+      CREATE TABLE categories
+      (
+          id          INTEGER     PRIMARY KEY,
+          name        TEXT        NOT NULL UNIQUE,
+          isIncome    INTEGER     NOT NULL CHECK (isIncome IN (0, 1)),
+          color       INTEGER
+      );
+
+      CREATE TABLE operations
+      (
+          id              INTEGER     PRIMARY KEY,
+          category_id     INTEGER     NOT NULL,
+          date            INTEGER     NOT NULL,
+          price           REAL        NOT NULL,
+
+          FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+      );
+    ''');
+  }
+
+  static Future<dynamic> _onConfigure(Database db) async
+  {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
   // Initializes and returns object for interaction with database.
   static Future<Database> getInstance() async
   {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String filepath = path.join(documentsDirectory.path, "TestDB.db");
-
+    await databaseFactory.deleteDatabase(await _getDatabaseFilepath());
     return await openDatabase(
-      filepath,
-      version: 1,
-      onCreate: (db, version) async
-      {
-        return await db.execute('''
-          CREATE TABLE categories
-          (
-              id          INTEGER     PRIMARY KEY,
-              name        TEXT        NOT NULL UNIQUE,
-              isIncome    INTEGER     NOT NULL CHECK (isIncome IN (0, 1)),
-              color       INTEGER
-          );
-
-          CREATE TABLE operations
-          (
-              id              INTEGER     PRIMARY KEY,
-              category_id     INTEGER     NOT NULL,
-              date            INTEGER     NOT NULL,
-              price           REAL        NOT NULL
-          );
-        ''');
-      }
+      await _getDatabaseFilepath(),
+      version: 3,
+      onCreate: _onCreate,
+      onConfigure: _onConfigure
     );
   }
 }
--- lib/repositories/operation_repository/operations_persistent_storage/operations_persistent_storage.dart
+++ lib/repositories/operation_repository/operations_persistent_storage/operations_persistent_storage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: curly_braces_in_flow_control_structures
 
+import 'package:sqflite/sqflite.dart';
 import '../DTO/operation_list_item.dart';
+import 'package:lab_money_5/database.dart';
 import '../view_models/operation_add_view_model.dart';
 import 'package:lab_money_5/repositories/operation_repository/view_models/operation_update_view_model.dart';
 
@@ -8,10 +10,20 @@ import 'package:lab_money_5/repositories/operation_repository/view_models/operat
 /// in a persistent storage (file or database).
 class OperationsPersistentStorage
 {
+  Database? _db;
+
+  bool _isInitialized()
+  {
+    return _db != null;
+  }
+
   /// Initializes [OperationsPersistentStorage] object
   Future<void> init() async
   {
-    // ...
+    if (_isInitialized())
+      return;
+
+    _db = await DB.getInstance();
   }
 
   /// Retrieves all existing user's financial operations
@@ -25,7 +37,7 @@ class OperationsPersistentStorage
   /// Adds a new financial operation for user.
   ///
   /// Nothing will be added if error was encountered.
-  Future<void> add(OperationAddViewModel category) async
+  Future<void> add(OperationAddViewModel operation) async
   {
     // ...
   }
@@ -33,7 +45,7 @@ class OperationsPersistentStorage
   /// Updates user's financial operation
   ///
   /// Nothing will be updated if error was encountered.
-  Future<void> update(OperationUpdateViewModel category) async
+  Future<void> update(OperationUpdateViewModel operation) async
   {
     // ...
   }
@@ -43,6 +55,17 @@ class OperationsPersistentStorage
   /// Nothing will be deleted if error was encountered or category not exists.
   Future<void> remove(int id) async
   {
-    // ...
+    if (! _isInitialized())
+      throw Exception('CategoriesPersistentStorage not initialized');
+
+    _db!.rawDelete(
+      '''
+        DELETE FROM
+            operations
+        WHERE
+            id = ?;
+      ''',
+      [id]
+    );
   }
 }
