eb995836ef9fba043f355d517db623c76f21abfd
cekator3 <hamsterdreams@inbox.ru>
2024-05-15 11:58:11 +0500

    ...

--- lib/UI/pages/statistics/widgets/category_type_buttons_widget.dart
+++ lib/UI/pages/statistics/widgets/category_type_buttons_widget.dart
@@ -1,13 +1,22 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'package:flutter/material.dart';
 import 'package:lab_money_5/enums/category_type.dart';
 
 /// A subsystem for displaying to the user
 /// buttons for selecting financial operations category type
-class CategoryTypeButtonsWidget extends StatelessWidget
+class CategoryTypeButtonsWidget extends StatefulWidget
 {
   final void Function(CategoryType) onCategoryTypeSelect;
 
   const CategoryTypeButtonsWidget({super.key, required this.onCategoryTypeSelect});
+
+  @override
+  State<CategoryTypeButtonsWidget> createState() => _CategoryTypeButtonsWidgetState();
+}
+
+class _CategoryTypeButtonsWidgetState extends State<CategoryTypeButtonsWidget> {
+  CategoryType _selectedCategoryType = CategoryType.loss;
 
   Widget _buildToggleButton(String label)
   {
@@ -24,8 +33,19 @@ class CategoryTypeButtonsWidget extends StatelessWidget
   Widget build(BuildContext context)
   {
     return ToggleButtons(
-      isSelected: [false, false],
-      onPressed: (index) => {},
+      isSelected: [_selectedCategoryType == CategoryType.income, _selectedCategoryType == CategoryType.loss],
+      onPressed: (index)
+      {
+        CategoryType type = index == 0
+                            ? CategoryType.income
+                            : CategoryType.loss;
+
+        setState(() {
+          _selectedCategoryType = type;
+        });
+
+        widget.onCategoryTypeSelect(type);
+      },
       children: [
         _buildToggleButton('Доходы'),
         _buildToggleButton('Расходы'),
